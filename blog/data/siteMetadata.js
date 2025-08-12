d9271d6923d306fd5f2beaf63bc848b5ee4f8647
Ljubo Lukovic <ljubolukovic93@gmail.com>
2025-08-12 17:27:28 +0200

    blog url updated

--- blog/data/siteMetadata.js
+++ blog/data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   language: 'en',
   locale: 'en',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://ljubo-blog.vercel.app/',
+  siteUrl: 'https://ljubo.rs',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
--- hub/lib/constants.ts
+++ hub/lib/constants.ts
@@ -1,3 +1,3 @@
 export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
-export const BLOG_URL = "https://ljubo-blog.vercel.app";
+export const BLOG_URL = "https://blog.ljubo.rs";
